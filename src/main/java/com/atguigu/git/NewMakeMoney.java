113268217e92f829362b493fe9b4fa1aaf8633f2
suyao <1521130274@qq.com>
2020-07-11 19:39:18 +0800

    how to make money

---
+++ src/main/java/com/atguigu/git/NewMakeMoney.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author suyso
+ * @create 2020-07-11 19:22
+ */
+public class NewMakeMoney {
+    public static void main(String[] args) {
+        System.out.println("Making money is too hard!!");
+    }
+}
