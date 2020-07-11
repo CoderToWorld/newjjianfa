ac7583a7f0572547b8f572ed87f26eab48742f0d
suyao <1521130274@qq.com>
2020-07-11 19:43:11 +0800

    how to make money

--- src/main/java/com/atguigu/git/NewMakeMoney.java
+++ src/main/java/com/atguigu/git/NewMakeMoney.java
@@ -7,5 +7,10 @@ package com.atguigu.git;
 public class NewMakeMoney {
     public static void main(String[] args) {
         System.out.println("Making money is too hard!!");
+
+        // TODO 红色 ： 未追踪代码 暂存区
+        // TODO 绿色 ： 未追踪代码首次进入暂存区
+        // TODO 黑色 ： 已提交本地库
+        // TODO 蓝色 ： 已提交本地库后 又发生了修改且未提交
     }
 }
